6f6314cb2a2b912827e54d58ab5ca08162e03d19
Raindrop <collins.odinga@student.moringaschool.com>
2022-03-02 23:51:57 +0300

    styled the body

---
+++ JS/index.js
@@ -0,0 +1,59 @@
+let data = 
+[
+    {
+        name: "mateo",
+        age: "30",
+    },
+    {
+        name: "sarah",
+        age: "32",
+    },
+    {
+        name: "John",
+        age: "20",
+    },
+    {
+        name: "Tim",
+        age: "27",
+    },
+    {
+        name: "Sam",
+        age: "22",
+    },
+    {
+        name: "Joey",
+        age: "34",
+    },
+    
+];
+
+
+const info = document.querySelector("#info")
+
+let details = data.map(function(item) {
+    return(
+        "<div>" + item.name + "" + " is " + item.age + " years old" +  "</div>"
+    )
+})
+
+
+info.innerHTML = details.join("\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
